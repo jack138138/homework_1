89567da5a454329fc4cb92f1f1913a3b3811088f
jack138138 <jackpeter138138@gmail.com>
2023-05-07 12:43:51 +0100

    Homework Submitted

---
+++ src/Class1/P1.java
@@ -0,0 +1,14 @@
+package Class1;
+
+public class P1 {
+    public static   void    main(String[]args) {
+
+
+        System.out.println("  /////");
+        System.out.println(" \"\"\"\"\"");
+        System.out.println("(| o   o |)");
+        System.out.println(" |   ^   |");
+        System.out.println(" | '-' |");
+        System.out.println(" +-----+");
+    }
+}
